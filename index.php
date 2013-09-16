6a655954528e28955ddae58c42468257d940d102
Adam Beerman <adam.beerman@gmail.com>
2013-09-16 19:19:34 -0400

    Added P1 homework content, and added the Bootstrap CSS links

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<h1>Welcome to P1!!</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>P1 - Adam Beerman</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <title>P1</title>
+        <div class = "table">
+            <table width = "600">
+                <tr>
+                    <td width = "300">
+                        <h1>Adam Beerman</h1>
+                    </td>
+                    <td width = "300">
+                        <img src ="img/pirate.jpg" width="300">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan = "2">
+                    <h2>Biography</h2>
+                    I am a chemical engineer by training, but I am always interested in learning something new. Previously, I worked as a process control engineer for Chevron, but am now working for a greenhouse start-up.
+                    <h2>Code Editor</h2>
+                    I will be using Sublime Text 2 as my code editor.
+                    </td>
+                </tr>
+            </table>
+        </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
