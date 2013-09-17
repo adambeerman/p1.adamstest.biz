1c322b7604b9f7a9dc947d3440e139f46e37a718
Adam Beerman <adam.beerman@gmail.com>
2013-09-16 21:51:16 -0400

    Last updates to the content of the index and css formatting

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
                 <td>
                 <h2>Biography</h2>
                 I am a chemical engineer by training, but I am always interested in learning something new. Previously, I worked as a process control engineer for Chevron, but am now working for a greenhouse start-up.
+                I got rid of my car 2 years ago and have never been happier!
                 <h2>Code Editor</h2>
                 I will be using Sublime Text 2 as my code editor.
                 </td>
